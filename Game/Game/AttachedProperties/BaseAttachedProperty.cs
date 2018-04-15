4a8c4be49db6b8bf7a03a5e7ba34d59cc1df572f
AlvexGR <goldenream1503@gmail.com>
2018-04-15 19:29:06 +0700

    fixed bugs

---
+++ Game/Game/AttachedProperties/BaseAttachedProperty.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+
+namespace Game
+{
+    public abstract class BaseAttachedProperty<Parent, Property>
+        where Parent : BaseAttachedProperty<Parent, Property>, new()
+    {
+        #region Public Properties
+
+        public static Parent Instance { set; private get; } = new Parent();
+
+        #endregion
+
+        #region Attached Property Definitions
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.RegisterAttached("Value", typeof(Property), typeof(BaseAttachedProperty<Parent, Property>), new PropertyMetadata(new PropertyChangedCallback(OnValuePropertyChanged)));
+        
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Instance.OnValueChanged(d, e);
+
+            Instance.ValueChanged(d, e);
+        }
+
+        public static Property GetValue(DependencyObject d)
+        {
+            return (Property)d.GetValue(ValueProperty);
+        }
+
+        public static void SetValue(DependencyObject d, Property value)
+        {
+            d.SetCurrentValue(ValueProperty, value);
+        }
+
+        #endregion
+
+        #region Public Events
+
+        public event Action<DependencyObject, DependencyPropertyChangedEventArgs> ValueChanged = (sender, e) => { };
+
+        #endregion
+
+        #region Event Methods
+
+        public virtual void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ Game/Game/AttachedProperties/RemoveNavigationFrameHistory.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Game
+{
+    public class RemoveNavigationFrameHistory : BaseAttachedProperty<RemoveNavigationFrameHistory, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            // get the frame
+            var frame = (sender as Frame);
+
+            // hidden the navigation bar
+            frame.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
+
+            // clear history
+            frame.Navigated += (ss, ee) => ((Frame)ss).NavigationService.RemoveBackEntry();
+
+        }
+    }
+}
